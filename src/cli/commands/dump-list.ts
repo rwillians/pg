0378a4c044e60692069556905edf0657288ada3f
Rafael Willians <me@rwillians.com>
2025-09-08 11:59:36 -0300

    Moved command dump-list to use db-v2

--- src/cli/commands/dump-list.ts
+++ src/cli/commands/dump-list.ts
@@ -1,14 +1,16 @@
 import { withHumanReadableSize } from '../../utils';
 import { $command } from '../commands';
-import { Dump } from '../../db-v1/dump';
+
+import { dumps } from '../../db-v2/dumps';
+import { from } from '../../db-v2';
 
 export const dumpList = $command({
   signature: 'ls',
   describe: 'Lists all dumps available',
   handler: async (_argv, ctx) => {
     const { db } = ctx;
-    const dumps = await Dump.all(db);
+    const rows = await from(dumps).all(db);
 
-    console.table(dumps.map(withHumanReadableSize));
+    console.table(rows.map(withHumanReadableSize));
   },
 });
