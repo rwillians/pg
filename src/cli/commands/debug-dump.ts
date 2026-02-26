7419e22d933a04df4c4b5201c522eac66b08d134
Rafael Willians <me@rwillians.com>
2026-02-26 12:13:37 -0300

    chore: prune

--- src/cli/commands/debug-dump.ts
+++ src/cli/commands/debug-dump.ts
@@ -1,9 +1,15 @@
 import { defineCommand } from '../command';
+import { inspect } from 'bun';
+
+/**
+ * @private Same as {@link inspect} but with hardcoded options.
+ * @since   18.0.0
+ * @version 1
+ */
+const i = (value: unknown) => inspect(value, { colors: true, compact: false, depth: Infinity, sorted: true });
 
 export const debugDump = defineCommand({
   signature: 'dump',
   description: 'Dumps all environment variables for debugging purposes',
-  handle: async () => {
-    console.log(process.env);
-  },
+  handle: async () => process.stdout.write(i(process.env) + '\n'),
 });
